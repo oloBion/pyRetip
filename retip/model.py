5b38d5e6f3da6bbb2873424937dbb198341457a1
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2021-03-08 17:36:16 +0100

    finished adding autogluon trainer

--- retip/model.py
+++ retip/model.py
@@ -20,9 +20,8 @@ class Trainer:
     def score(self, X_test=None, y_test=None):
         pass
 
-    @abc.abstractmethod
-    def predict(self):
-        pass
+    def predict(self, X):
+        return self.model.predict(X)
 
 
 class XGBoostTrainer(Trainer):
@@ -66,7 +65,7 @@ class XGBoostTrainer(Trainer):
             y = test_data.RT.values
         
 
-        y_pred = self.model.predict(X)
+        y_pred = self.predict(X)
         rt_error = y - y_pred
 
         return {
@@ -80,5 +79,48 @@ class XGBoostTrainer(Trainer):
             '95_percent_confidence_interval': st.norm.ppf(0.95, loc=np.mean(rt_error), scale=np.std(rt_error))
         }
 
-    def predict(self, X):
-        return self.model.predict(X)
+
+class AutoGluonTrainer(Trainer):
+    def __init__(self, dataset: Dataset, training_duration: int = 60, n_cpu: int = 4):
+        self.dataset = dataset
+        self.training_duration = training_duration
+        self.n_cpu = n_cpu
+
+        self.model = TabularPredictor(label=Dataset.RT_COLUMN)
+
+    def train(self):
+        t = time.time()
+
+        self.model.fit(
+            train_data=self.dataset.get_training_data(),
+            time_limit=60 * self.minutes,
+            preset='good_quality_faster_inference_only_refit',
+            num_cpus=self.n_cpu
+        )
+
+        elapsed_time = str(datetime.timedelta(seconds=time.time() - t))
+
+        fit_summary = model.fit_summary(verbosity=0)
+        best_score = fit_summary['leaderboard'].loc[0, 'score']
+
+        print(f'Training completed in {elapsed_time} with best RMSE {best_score:.3f}')
+
+    def score(self, X=None, y=None):
+        if not X or not y:
+            test_data = self.dataset.get_test_data()
+            X = test_data.drop('RT', axis=1)
+            y = test_data.RT.values
+
+        y_pred = self.predict(X)
+        rt_error = y - y_pred
+
+        return {
+            'root_mean_squared_error': metrics.mean_squared_error(y, y_pred, squared=True),
+            'mean_absolute_error': metrics.mean_absolute_error(y, y_pred),
+            'explained_variance_score': metrics.explained_variance_score(y, y_pred),
+            'r2_score': metrics.r2_score(y, y_pred),
+            'pearson_correlation': st.pearsonr(y, y_pred),
+            'mean_squared_error': metrics.mean_squared_error(y, y_pred),
+            'median_absolute_error': metrics.median_absolute_error(y, y_pred),
+            '95_percent_confidence_interval': st.norm.ppf(0.95, loc=np.mean(rt_error), scale=np.std(rt_error))
+        }
